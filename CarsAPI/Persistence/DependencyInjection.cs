67a5c3a038b372dece78d7584135b41b932b2603
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2022-10-04 14:29:12 +0300

    add dependency injection (Applcation and Persistence)

---
+++ CarsAPI/Persistence/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using CarsServer.Data;
+using Applicaton.Interfaces;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<DataContext>(options =>
+            {
+                //options.UseSqlite(connectionString);
+                options.UseSqlServer(connectionString);
+                //options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<IDataContext>(provider =>
+                provider.GetService<DataContext>());
+
+            return services;
+        }
+    }
+}
