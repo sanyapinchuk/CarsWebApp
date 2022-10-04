76f702c597f6bf3697105b0cfb032ea06514c916
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2022-10-04 03:49:16 +0300

    add get short info car list

---
+++ CarsAPI/Applicaton/Cars/Queries/GetCarsList/GetCarListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿
+
+using MediatR;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Applicaton.Interfaces;
+
+namespace Applicaton.Cars.Queries.GetCarsList
+{
+    public class GetCarListQueryHandler
+        : IRequestHandler<GetCarListQuery, CarListDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IDataContext _dataContext;
+        public GetCarListQueryHandler(IDataContext dataContext, IMapper mapper)
+        {
+            _mapper = mapper;
+            _dataContext = dataContext;
+        }
+        public async Task<CarListDto> Handle(GetCarListQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var cars = await _dataContext.Cars
+                .ProjectTo<CarLookupVm>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+            return new CarListDto() { Cars = cars };
+        }
+    }
+}
