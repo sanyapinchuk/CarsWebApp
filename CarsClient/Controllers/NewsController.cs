386f3373ff9c4a59ebc23b4513922548604fa33d
alexandrPinchuk <a.pinchuk@launchoptionsltd.com>
2024-03-11 00:27:35 +0300

    add news and deploy

---
+++ CarsClient/Controllers/NewsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarsClient.Controllers
+{
+    [Route("all-news")]
+    public class NewsController : Controller
+    {
+	    private GlobalVariables _globalVariables;
+	    private readonly IConfiguration _configuration;
+	    public NewsController(GlobalVariables globalVariables,
+		    IConfiguration configuration)
+	    {
+		    _globalVariables = globalVariables;
+		    _configuration = configuration;
+	    }
+
+		[Route("")]
+        public async Task<IActionResult> News()
+        {
+	        ViewData["Postfix"] = _globalVariables.Postfix;
+			return View();
+        }
+
+        [Route("{newsName}")]
+        public IActionResult GetNews([FromRoute] string newsName)
+        {
+	        ViewData["Postfix"] = _globalVariables.Postfix;
+			return View(newsName);
+        }
+    }
+}
